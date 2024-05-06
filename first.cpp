13aaeb8d762b3c4d39257f2e77dfe3f1ae2be9bf
Rupaksingh17 <168707974+Rupaksingh17@users.noreply.github.com>
2024-05-06 11:11:14 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include<iostream     >
+using namespace std;
+int main()
+{
+    cout<<"Alphabets:abcdefghijklmnopqrstuvwxyz"<<endl;
+    cout<<"Numbers:0123456789"<<endl;
+    cout<<"Special characters:!@#$%^&*";
+    return 0;
+}
