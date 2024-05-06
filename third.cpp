f22dc74cd1198b6fed4ad8bd718624bcd5996bcc
Rupaksingh17 <168707974+Rupaksingh17@users.noreply.github.com>
2024-05-06 11:56:20 +0530

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a and b"<<endl;
+    cin>>a>>b;
+//Airthmetic operators
+    cout<<"Addition:"<<a+b<<endl;
+    cout<<"Subtraction:"<<a-b<<endl;
+    cout<<"Multiplication:"<<a*b<<endl;
+    cout<<"Division:"<<a/b<<endl;
+    cout<<"Modulo:"<<a%b<<endl;
+    return 0;
+}
