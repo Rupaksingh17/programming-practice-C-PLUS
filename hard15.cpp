d01c5e71b2bd523c811d60612d98eec01221ee0c
Rupaksingh17 <168707974+Rupaksingh17@users.noreply.github.com>
2026-02-15 23:02:11 +0530

    Add files via upload

---
+++ hard15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the elements: ";
+    cin>>n;
+    if(n>=30)
+    {
+        cout<<"PASS";
+    }
+    else {
+        cout<<"FAIL";
+    }
+}
