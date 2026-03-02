f69dcb2522100c269cf9da3053e8bb3f4d76e7d2
Rupaksingh17 <168707974+Rupaksingh17@users.noreply.github.com>
2026-03-03 02:34:17 +0530

    Add files via upload

---
+++ function6.cpp
@@ -0,0 +1,17 @@
+ #include <iostream>
+ using namespace std;
+ int trailingzeroes (int N)
+ {
+    int count=0;
+    while(N>5)
+    {
+        count += N/5;
+        N/=5;
+
+    }
+    return count;
+ } 
+ int main()
+ {
+
+ }
