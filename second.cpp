8c8dace8d6005269e6ed3fa9b0c3cf9fdff5982a
Rupaksingh17 <168707974+Rupaksingh17@users.noreply.github.com>
2026-03-05 02:47:35 +0530

    Add files via upload

--- second.cpp
+++ second.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+# include <iostream>
 using namespace std;
 int main()
 {
-    int age;
-    cout<<"Enter your age"<<endl;
-    cin>>age;
-    cout<<"Month: "<<12*age<<endl;
-    cout<<"Days: "<<365*age<<endl;
-    return 0;
+    int a,b;
+    cout<<"Enter the first number: ";
+    cin>>a;
+    cout<<"Enter the Second number: ";
+    cin>>b;
+    cout<<"Sum of these numbers: "<<a+b;
 }
